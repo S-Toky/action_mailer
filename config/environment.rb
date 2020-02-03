5e1a80173a1f03d6be01011c7146ca828e2faa0d
S-Toky <stokyr29@gmail.com>
2020-02-03 13:25:13 +0300

    add this

--- config/environment.rb
+++ config/environment.rb
@@ -10,5 +10,6 @@ ActionMailer::Base.smtp_settings ={
 :domain => 'gmail.com', #you can also use google.com
 :authentication => :plain,
 :user_name => ENV['GMAIL_LOGIN'],
-:password => ENV['GMAIL_PWD']
+:password => ENV['GMAIL_PWD'],
+:enable_starttls_auto => true
 }
